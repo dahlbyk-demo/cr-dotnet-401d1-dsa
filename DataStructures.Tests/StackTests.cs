674eec47121bd1216eb91890f92bcb389faae65e
Keith Dahlby <keith@newbo.co>
2020-05-15 14:18:26 -0500

    Scaffold a Stack<T> class

---
+++ DataStructures.Tests/StackTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void Peek_empty_stack_throws()
+        {
+            // Arrange
+            Stack<object> stack = new Stack<object>();
+
+            // Assert
+            Assert.Throws<StackEmptyException>(() =>
+            {
+                // Act
+                stack.Peek();
+            });
+        }
+    }
+}
